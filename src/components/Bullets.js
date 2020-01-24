d3c41e63727caf74384109e3554584717dedb178
Michaela Lehr <michi_lehr@web.de>
2020-01-24 12:43:41 +0100

    ADD sticky arrows

--- src/components/Bullets.js
+++ src/components/Bullets.js
@@ -34,27 +34,31 @@ export default class Bullets extends Component {
         <div className="bullets__container">
           <span id="sticky-bullets" className="text__sticky">Leistungsspektrum</span>
           <div id="bullets-title" className="bullets__title-container">
-            <h1 className="bullets__headline">Hier findest Du eine Auswahl der Themen, die wir gemeinsam in Workshop-Form oder prozessual bearbeiten können. Sprich mich gerne an, wenn das was Du suchst noch nicht dabei ist.</h1>
-            <div className="list__container list__container--first">
-              <h3 className="list__headline">Für Menschen und Marken:</h3>
-              <ul>
-                <li className="list__element">Visions-Entwicklung und Ziel-Definition</li>
-                <li className="list__element">Strategie - und Positionierungsfragen</li>
-                <li className="list__element">Markenentwicklung und -workshops</li>
-                <li className="list__element">Umfeld- und Zielgruppenanalysen</li>
-                <li className="list__element">Ideen und Konzeption</li>
-                <li className="list__element">Content-Strategie und Storytelling</li>
-              </ul>
-            </div>
-            <div className="list__container list__container--second">
-              <h3 className="list__headline">Für Agenturen und Beratungen:</h3>
-              <ul>
-                <li className="list__element">Pitch-Lead</li>
-                <li className="list__element">Team-Lead</li>
-                <li className="list__element">Budgetierung und Projektplanung</li>
-                <li className="list__element">Interims-Vertretung</li>
-              </ul>
-            </div>
+            <h1 className="bullets__headline">
+              Hier findest Du eine Auswahl der Themen, die wir gemeinsam in Workshop-Form oder prozessual bearbeiten können. Sprich mich gerne an, wenn das was Du suchst noch nicht dabei ist.
+            </h1>
+          </div>
+          <span className="arrow__sticky"></span>
+          <div className="list__container list__container--first">
+            <h3 className="list__headline">Für Menschen und Marken:</h3>
+            <ul>
+              <li className="list__element">Visions-Entwicklung und Ziel-Definition</li>
+              <li className="list__element">Strategie - und Positionierungsfragen</li>
+              <li className="list__element">Markenentwicklung und -workshops</li>
+              <li className="list__element">Umfeld- und Zielgruppenanalysen</li>
+              <li className="list__element">Ideen und Konzeption</li>
+              <li className="list__element">Content-Strategie und Storytelling</li>
+            </ul>
+          </div>
+          <span className="list__container__arrow"></span>
+          <div className="list__container list__container--second">
+            <h3 className="list__headline">Für Agenturen und Beratungen:</h3>
+            <ul>
+              <li className="list__element">Pitch-Lead</li>
+              <li className="list__element">Team-Lead</li>
+              <li className="list__element">Budgetierung und Projektplanung</li>
+              <li className="list__element">Interims-Vertretung</li>
+            </ul>
           </div>
         </div>
       </>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -42,27 +42,33 @@ export default class Gallery extends Component {
 
   render() {
     return (
-      <div className="tiles">
-        <span id="sticky-about" className="text__sticky">Über mich</span>
-        <div className="tile tile--second">
-          <img className="tile__image" src={portrait}/>
+      <>
+        <div className="tiles">
+          <span id="sticky-about" className="text__sticky">Über mich</span>
+          <span className="arrow__sticky arrow__sticky--gallery"></span>
+          <div className="tile tile--first">
+            <img className="tile__image" src={portrait}/>
+          </div>
+          <div id="about-title" className="tile tile--text tile--second">
+            <h3 className="tile__headline">Sarah</h3>
+            <p>Strategisches und kreatives Arbeiten braucht geistige Kapazitäten. Als fast schon Digital Native habe ich gleichzeitig gerade noch ein Magister-Studium in der Tasche: Mit Musikwissenschaft, Soziologie und Psychologie habe ich unterschiedliche Ansätze und Methoden studiert aber vor allem gelernt, analytisch und interdisziplinär zu denken. Als nicht nur Kopf-Mensch interessiere ich mich genauso für Körper &amp; Seele. Für den ganzen Menschen - und für die Menschen um mich herum. Für Musik, Yoga, gesundes Essen und das Schöne Leben.</p>
+            <p>
+              <a className="tile__link text__underlined text__underlined--fine">Kontakt</a>
+            </p>
+          </div>
         </div>
-        <div id="about-title" className="tile tile--text tile--first">
-          <h3 className="tile__headline">Sarah</h3>
-          <p>Strategisches und kreatives Arbeiten braucht geistige Kapazitäten. Als fast schon Digital Native habe ich gleichzeitig gerade noch ein Magister-Studium in der Tasche: Mit Musikwissenschaft, Soziologie und Psychologie habe ich unterschiedliche Ansätze und Methoden studiert aber vor allem gelernt, analytisch und interdisziplinär zu denken. Als nicht nur Kopf-Mensch interessiere ich mich genauso für Körper &amp; Seele. Für den ganzen Menschen - und für die Menschen um mich herum. Für Musik, Yoga, gesundes Essen und das Schöne Leben.</p>
-          <p>
-            <a className="tile__link text__underlined text__underlined--fine">Kontakt</a>
-          </p>
+        <div className="tiles">
+          <span id="sticky-network" className="text__sticky">Mein Netzwerk</span>
+          <span className="arrow__sticky arrow__sticky--gallery"></span>
+          <div className="tile tile--text tile--third">
+            <h3 className="tile__headline">Lieblingskollegen</h3>
+            <p id="network-title">Ich arbeite am liebsten im Team und habe ein Netzwerk aus richtig guten Kollegen, das ich gerne mit in unser Projekt hole. Dazu zählen neben Strategie, Kreativdirektion, Art Direktion und Redaktion auch technische Services wie Web-Development, UI/UX oder Contentproduktion.</p>
+          </div>
+          <div className="tile tile--last--grid">
+            <img className="tile__image" src={landscape}/>
+          </div>
         </div>
-        <span id="sticky-network" className="text__sticky">Mein Netzwerk</span>
-        <div className="tile tile--third--grid">
-          <img className="tile__image" src={landscape}/>
-        </div>
-        <div className="tile tile--text tile--last">
-          <h3 className="tile__headline">Lieblingskollegen</h3>
-          <p id="network-title">Ich arbeite am liebsten im Team und habe ein Netzwerk aus richtig guten Kollegen, das ich gerne mit in unser Projekt hole. Dazu zählen neben Strategie, Kreativdirektion, Art Direktion und Redaktion auch technische Services wie Web-Development, UI/UX oder Contentproduktion.</p>
-        </div>
-      </div>
+      </>
     )
   }
 }
