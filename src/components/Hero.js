b9669baa4645a582bd9e008544b9c66765d0a2b1
Michaela Lehr <michi_lehr@web.de>
2020-01-24 17:44:03 +0100

    MOD layout fixes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -39,7 +39,7 @@ export default class Hero extends Component {
       <>
         <span className="text__sticky">{content.tagline}</span>
         <div className="hero__container">
-          <h1>Hallo, ich bin <span onClick={this.scrollToAbout} className="text__underlined">Sarah</span>.<br/>Ich biete <span className="text__underlined">kreative Strategie</span> und <span className="text__underlined">Beratung</span> für <span className="text__underlined">Menschen</span> und <span className="text__underlined">Marken</span> an. Gemeinsam arbeiten wir an <span className="text__underlined">Positionierungsfragen</span>, <span className="text__underlined">Visionen</span> und <span className="text__underlined">Zielen</span> und daran, wie der <span className="text__underlined">Weg dorthin</span> aussehen kann.</h1>
+          <h1 className="hero__container__headline">Hallo, ich bin <span onClick={this.scrollToAbout} className="text__underlined">Sarah</span>.<br/>Ich biete <span className="text__underlined">kreative Strategie</span> und <span className="text__underlined">Beratung</span> für <span className="text__underlined">Menschen</span> und <span className="text__underlined">Marken</span> an. Gemeinsam arbeiten wir an <span className="text__underlined">Positionierungsfragen</span>, <span className="text__underlined">Visionen</span> und <span className="text__underlined">Zielen</span> und daran, wie der <span className="text__underlined">Weg dorthin</span> aussehen kann.</h1>
         </div>
         <span className="animated-line"></span>
       </>
